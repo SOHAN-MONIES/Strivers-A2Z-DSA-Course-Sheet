c26972d337ec6390b852a87bc1b736ae52c7d436
SOHAN-MONIES <anon.marvin.sohan@gmail.com>
2025-02-21 00:41:16 +0530

    solved few more

---
+++ BinarySearch/medium/Find_nth_root_of_m.cpp
@@ -0,0 +1,33 @@
+// URL: https://www.geeksforgeeks.org/problems/find-nth-root-of-m5843/1
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int nthRoot(int n, int m)
+{
+    int l = 1;
+    int h = m;
+    while (l <= h)
+    {
+        int mid= (l + h) / 2;
+        if (pow(mid, n) == m)
+        {
+            return mid;
+        }
+        if (pow(mid, n) > m)
+        {
+            h = mid- 1;
+        }
+        else
+        {
+            l = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    cout << nthRoot(2, 9);
+    return 0;
+}
