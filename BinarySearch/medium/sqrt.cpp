e564e23a7a9bfe335fdba5097271bc680299ee93
SOHAN-MONIES <anon.marvin.sohan@gmail.com>
2025-02-20 23:56:42 +0530

    solved few more

--- BinarySearch/medium/sqrt.cpp
+++ BinarySearch/medium/sqrt.cpp
@@ -1,10 +1,31 @@
-// URL: 
+// URL: https://leetcode.com/problems/sqrtx/
 #include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
 
+int mySqrt(int x)
+{
+    int l = 1;
+    int h = x;
+    int ans = 0;
+    while (l <= h)
+    {
+        int m = l + (h - l) / 2;
+        if (m <= x / m)
+        {
+            ans = m;
+            l = m + 1;
+        }
+        else
+        {
+            h = m - 1;
+        }
+    }
+    return ans;
+}
 
-int main() {
-    
+int main()
+{
+    cout << mySqrt(17);
     return 0;
 }
