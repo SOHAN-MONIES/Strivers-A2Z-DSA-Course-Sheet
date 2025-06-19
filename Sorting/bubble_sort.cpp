8dae405f1b1da448daacfd70ca6a2fce7b11c2e9
SOHAN-MONIES <anon.marvin.sohan@gmail.com>
2025-06-19 12:05:56 +0530

    message

--- Sorting/bubble_sort.cpp
+++ Sorting/bubble_sort.cpp
@@ -2,8 +2,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
+// PUSH THE MAX TO LAST BY ADJACENT SWAPS
+void bubbleSort(vector<int> &arr)
 {
 
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++)
+    {
+        int swap_flag = 0;
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                swap_flag++;
+            }
+        }
+        if (swap_flag == 0)
+        {
+            break;
+        }
+    }
+}
+
+void printArr(vector<int> arr)
+{
+    for (auto num : arr)
+    {
+        cout << num << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    vector<int> arr = {13, 46, 24, 52, 30, 9};
+    printArr(arr);
+    bubbleSort(arr);
+    printArr(arr);
     return 0;
 }
--- Sorting/selection_sort.cpp
+++ Sorting/selection_sort.cpp
@@ -1,9 +1,39 @@
-// URL: https://www.geeksforgeeks.org/problems/selection-sort/1
+// URL: https://www.geeksforgeeks.org/problems/insertion-sort/1
 #include <bits/stdc++.h>
 using namespace std;
 
+// Search for minimum element from beginning and swap it with beginning element
+
+void selectionSort(vector<int> &arr)
+{
+    int n = arr.size();
+    int min_index;
+    for (int i = 0; i < n - 1; i++)
+    {
+        min_index = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_index])
+                min_index = j;
+        }
+        swap(arr[i], arr[min_index]);
+    }
+}
+
+void printArr(vector<int> arr)
+{
+    for (auto num : arr)
+    {
+        cout << num << " ";
+    }
+    cout << endl;
+}
+
 int main()
 {
-
+    vector<int> arr = {13, 46, 24, 52, 30, 9};
+    printArr(arr);
+    selectionSort(arr);
+    printArr(arr);
     return 0;
 }
