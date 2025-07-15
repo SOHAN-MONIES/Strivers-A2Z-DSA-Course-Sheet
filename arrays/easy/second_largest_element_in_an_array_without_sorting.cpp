6cb9d8df2aae6ed7488899d69ed95bb1d9df8ef1
Sohan Monies <108331198+SOHAN-MONIES@users.noreply.github.com>
2025-07-15 12:27:40 +0530

    Update second_largest_element_in_an_array_without_sorting.cpp

--- arrays/easy/second_largest_element_in_an_array_without_sorting.cpp
+++ arrays/easy/second_largest_element_in_an_array_without_sorting.cpp
@@ -2,23 +2,34 @@
 #include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
+/*Logic to Find Second Largest in Array
 
-int SecondSmallest(vector<int> arr)
-{
-    int smallest = INT_MAX;
-    int SecondSmallest = INT_MAX;
-    for (auto num : arr)
-    {
-        smallest = min(smallest, num);
-    }
-    for (auto num : arr)
-    {
-        if (num > smallest && num < SecondSmallest)
-            SecondSmallest = num;
-    }
-    return SecondSmallest;
-}
+Initialize:
+- Set large to the first element.
+- Set slarge (second largest) to -1.
 
+Traverse the array:
+- If you find a new number greater than large:
+    - Update slarge = large.
+    - Update large = current element.
+- Else if the current element is between large and slarge (i.e. larger than slarge but less than large):
+    - Update slarge = current element.*/
+
+ int getSecondLargest(vector<int> &arr) {
+       int n=arr.size();
+       int large=arr[0];
+       int slarge=-1;
+       for(int i=1;i<n;i++){
+           if(arr[i]>large){
+               slarge=large;
+               large=arr[i];
+           }
+           else if(arr[i]>slarge&&arr[i]<large){
+               slarge=arr[i];
+           }
+       }
+       return slarge;
+    }
 int main()
 {
 
