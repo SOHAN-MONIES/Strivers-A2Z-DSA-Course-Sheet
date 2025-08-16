66e72cdd7900b199576969db0142daed256024a3
SOHAN-MONIES <anon.marvin.sohan@gmail.com>
2025-08-16 20:03:32 +0530

    message

---
+++ Recursion.cpp/easy/fibo.cpp
@@ -0,0 +1,16 @@
+// URL:
+#include <bits/stdc++.h>
+using namespace std;
+
+int fib(int n)
+{
+    if (n < 2)
+        return n;
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+    int a = fib(4);
+    return 0;
+}
