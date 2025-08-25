f4f17c1f0c733b26e7278fe08b14949aad22c73c
SOHAN-MONIES <anon.marvin.sohan@gmail.com>
2025-08-25 21:23:59 +0530

    message

---
+++ Bit_Manipulation/Medium/single_number.cpp
@@ -0,0 +1,19 @@
+// URL:https://leetcode.com/problems/single-number/
+#include <bits/stdc++.h>
+using namespace std;
+
+int singleNumber(vector<int> &nums)
+{
+    int res = nums[0];
+    for (int i = 1; i < nums.size(); i++)
+    {
+        res ^= nums[i];
+    }
+
+    return res;
+}
+int main()
+{
+
+    return 0;
+}
