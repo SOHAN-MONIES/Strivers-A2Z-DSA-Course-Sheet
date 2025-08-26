7b6ce2736c2df0f58e27fac7b88c9f950ab5de57
SOHAN-MONIES <anon.marvin.sohan@gmail.com>
2025-08-26 13:16:41 +0530

    message

---
+++ Trees/traversals/postorder.cpp
@@ -0,0 +1,46 @@
+// URL:https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x,TreeNode* left,TreeNode* right): val(x),left(left),right(right) {}
+};
+
+void preorder_helper(TreeNode* root, vector<int>& ans) {
+    if (!root) return;
+
+    preorder_helper(root->left, ans);
+    preorder_helper(root->right, ans);
+    ans.push_back(root->val);
+}
+
+vector<int> postorderTraversal(TreeNode* root) {
+    vector<int> ans;
+    preorder_helper(root, ans);
+    return ans;
+}
+
+
+
+
+int main() {
+    TreeNode* root=new TreeNode(1);
+    root->left=new TreeNode(2);
+    root->right=new TreeNode(3);
+    root->left->left=new TreeNode(4);
+    root->left->right=new TreeNode(5);
+    root->left->right->left=new TreeNode(8);
+    root->right->left=new TreeNode(6);
+    root->right->right=new TreeNode(7);
+    root->right->right->left=new TreeNode(9);
+    root->right->right->right=new TreeNode(10);
+    vector<int>ans=postorderTraversal(root);
+    for (auto ele:ans) {
+        cout<<ele<<endl;
+    }
+    return 0;
+}
